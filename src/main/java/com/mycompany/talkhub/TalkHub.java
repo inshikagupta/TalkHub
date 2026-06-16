f9888775d34f6d5658ba3ad8fc4d59e5de3d389a
Inshika Gupta <inshika@Inshikas-MacBook-Air.local>
2026-06-16 19:45:44 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/talkhub/TalkHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.talkhub;
+
+/**
+ *
+ * @author inshika
+ */
+public class TalkHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
