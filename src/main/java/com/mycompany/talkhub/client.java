95076f416f8a1c85ae855fe172148021ce8beb94
ShubhamP08 <shubhamprajapati511@gmail.com>
2026-06-16 19:59:48 +0530

    check1

---
+++ src/main/java/com/mycompany/talkhub/client.java
@@ -0,0 +1,14 @@
+package com.mycompany.talkhub;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author shubhamp
+ */
+public class client {
+    String str="";
+}
